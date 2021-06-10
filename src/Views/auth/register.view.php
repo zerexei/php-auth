f6231fde52ead401ab13cbeb858087db6f9d4ae2
Angelo Arcillas <angeloarcillas64@gmail.com>
2021-06-10 11:25:32 +0800

    Separate header and footer

--- src/Views/auth/register.view.php
+++ src/Views/auth/register.view.php
@@ -1,62 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php includes('header') ?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+<div class="w-1/3 p-6 mx-auto bg-white shadow rounded">
+  <!-- LABEL -->
+  <h2 class="mb-6 text-xl font-bold text-blue-400 text-center">Register</h2>
 
-  <!-- FONTS -->
-  <link rel="preconnect" href="https://fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+  <!-- ERROR FEEDBACK -->
+  <?php includes('errors') ?>
 
-  <!-- STYLESHEET -->
-  <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
-</head>
+  <form action="/php-auth/register" method="POST">
+    <!-- USERNAME -->
+    <div class="mb-6">
+      <label for="Email" class="block mb-1 text-sm font-bold text-gray-500">Email</label>
+      <input type="text" name="email" placeholder="Email..." id="Email" class="w-full p-2 text-gray-700 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+    </div>
+    <!-- PASSWORD -->
+    <div class="mb-6">
+      <label for="Password" class="block mb-1 text-sm font-bold text-gray-500">Password</label>
+      <input type="text" name="password" placeholder="Password..." id="Password" class="w-full p-2 text-gray-700 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+    </div>
+    <!-- CONFIRM PASSWORD -->
+    <div class="mb-6">
+      <label for="password_confirmation" class="block mb-1 text-sm font-bold text-gray-500">Confirm password</label>
+      <input type="text" name="password_confirmation" placeholder="confirm password..." id="password_confirmation" class="w-full p-2 text-gray-700 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
+    </div>
+    <!-- SUBMIT -->
+    <div class="mb-6">
+      <button class="w-full py-2 px-3 text-white bg-blue-400 rounded hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50">Register</button>
+    </div>
+  </form>
+</div>
+</div>
 
-<style>
-  * {
-    font-family: 'Open Sans', sans-serif;
-  }
-</style>
-
-<body>
-  <div class="p-12 bg-gray-50 min-h-screen">
-    <main>
-
-      <div class="w-1/3 p-6 mx-auto bg-white shadow rounded">
-        <!-- LABEL -->
-        <h2 class="mb-6 text-xl font-bold text-blue-400 text-center">Register</h2>
-
-        <?php includes('errors') ?>
-
-        <form action="/php-auth/register" method="POST">
-          <!-- USERNAME -->
-          <div class="mb-6">
-            <label for="Email" class="block mb-1 text-sm font-bold text-gray-500">Email</label>
-            <input type="text" name="email" placeholder="Email..." id="Email" class="w-full p-2 text-gray-700 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-          </div>
-          <!-- PASSWORD -->
-          <div class="mb-6">
-            <label for="Password" class="block mb-1 text-sm font-bold text-gray-500">Password</label>
-            <input type="text" name="password" placeholder="Password..." id="Password" class="w-full p-2 text-gray-700 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-          </div>
-          <!-- CONFIRM PASSWORD -->
-          <div class="mb-6">
-            <label for="password_confirmation" class="block mb-1 text-sm font-bold text-gray-500">Confirm password</label>
-            <input type="text" name="password_confirmation" placeholder="confirm password..." id="password_confirmation" class="w-full p-2 text-gray-700 border rounded focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50">
-          </div>
-          <!-- SUBMIT -->
-          <div class="mb-6">
-            <button class="w-full py-2 px-3 text-white bg-blue-400 rounded hover:bg-blue-500 focus:outline-none focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50">Register</button>
-          </div>
-        </form>
-
-      </div>
-
-  </div>
-  </main>
-</body>
-
-</html>
+<?php includes('header') ?>
---
+++ src/Views/includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <!-- FONTS -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap" rel="stylesheet">
+
+    <!-- STYLESHEET -->
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+
+<style>
+    * {
+        font-family: 'Open Sans', sans-serif;
+    }
+</style>
+
+<body>
+    <div class="p-12 bg-gray-50 min-h-screen">
+        <main>
